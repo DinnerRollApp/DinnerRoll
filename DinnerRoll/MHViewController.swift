6030c867adb47adb7f6f2b78e6535d3527167000
Michael Hulet <bestraysfan@me.com>
2017-04-27 22:27:06 -0700

    Everything technically works rn I think

--- DinnerRoll/MHViewController.swift
+++ DinnerRoll/MHViewController.swift
@@ -8,18 +8,24 @@
 
 import UIKit
 import MapKit
+import SwiftyJSON
 import QuadratTouch
 import INTULocationManager
 
-class MHViewController: UIViewController, MKMapViewDelegate{
+class MHViewController: UIViewController, MKMapViewDelegate, DBMapSelectorManagerDelegate{
     @IBOutlet weak var restaurantLabel: UILabel!
     @IBOutlet weak var spinner: UIActivityIndicatorView!
     @IBOutlet weak var refreshButton: UIButton!
     @IBOutlet weak var map: MKMapView!
+    var selectionCircle: DBMapSelectorManager? = nil
     override func viewDidLoad() -> Void{
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         map.delegate = self
+        selectionCircle = DBMapSelectorManager(mapView: map)
+        selectionCircle?.delegate = self
+        selectionCircle?.editingType = .full
+        selectionCircle?.circleRadius = 1000
         refresh()
     }
     @IBAction func refresh() -> Void{
@@ -30,38 +36,54 @@ class MHViewController: UIViewController, MKMapViewDelegate{
             guard let currentLocation = location, accuracy.rawValue >= INTULocationAccuracy.block.rawValue else{
                 return
             }
-            let radius: Double = 1000
-            let search = Session.sharedSession().venues.search([QuadratTouch.Parameter.ll: "\(currentLocation.coordinate.latitude),\(currentLocation.coordinate.longitude)", QuadratTouch.Parameter.llAcc: "\(currentLocation.horizontalAccuracy)", QuadratTouch.Parameter.alt: "\(currentLocation.altitude)", QuadratTouch.Parameter.altAcc: "\(currentLocation.verticalAccuracy)", QuadratTouch.Parameter.categoryId: "4d4b7105d754a06374d81259", QuadratTouch.Parameter.radius: String(radius), QuadratTouch.Parameter.limit: "50"], completionHandler:{(result: QuadratTouch.Result) in
+            self.map.removeAnnotations(self.map.annotations)
+            //self.map.removeOverlays(self.map.overlays)
+            if self.selectionCircle!.circleCoordinate == CLLocationCoordinate2D(){
+                self.selectionCircle?.circleCoordinate = currentLocation.coordinate
+            }
+            self.selectionCircle?.applySelectorSettings()
+            let search = Session.sharedSession().venues.search([QuadratTouch.Parameter.ll: "\(self.selectionCircle!.circleCoordinate.latitude),\(self.selectionCircle!.circleCoordinate.longitude)", QuadratTouch.Parameter.llAcc: "\(currentLocation.horizontalAccuracy)", QuadratTouch.Parameter.alt: "\(currentLocation.altitude)", QuadratTouch.Parameter.altAcc: "\(currentLocation.verticalAccuracy)", QuadratTouch.Parameter.categoryId: "4d4b7105d754a06374d81259" , QuadratTouch.Parameter.radius: String(self.selectionCircle!.circleRadius), QuadratTouch.Parameter.limit: "50"], completionHandler:{(result: QuadratTouch.Result) in
                 guard let response = result.response else{
                     return
                 }
+                let data = JSON(response)
                 //To display a full circle, you have to display the diameter, plus a little padding to show the whole pin
-                let mapSize = radius * 2 + 500
-                self.map.setRegion(MKCoordinateRegionMakeWithDistance(currentLocation.coordinate, mapSize, mapSize), animated: true)
-                self.map.removeAnnotations(self.map.annotations)
-                self.map.removeOverlays(self.map.overlays)
-                let restaurant = (response["venues"]! as! [[String: Any]]).randomElement
-                print(restaurant)
+                let restaurant = data["venues"].array!.randomElement
+                //print(restaurant)
                 let annotation = MKPointAnnotation()
-                annotation.coordinate = CLLocationCoordinate2D(latitude: ((restaurant["location"] as! [String: Any])["lat"] as! NSNumber).doubleValue, longitude: ((restaurant["location"] as! [String: Any])["lng"] as! NSNumber).doubleValue)
+                annotation.coordinate = CLLocationCoordinate2D(latitude: restaurant["location"]["lat"].double!, longitude: restaurant["location"]["lng"].double!)
                 self.map.addAnnotation(annotation)
-                let circle = MKCircle(center: currentLocation.coordinate, radius: radius)
-                self.map.add(circle, level: .aboveRoads)
-                self.restaurantLabel.text = restaurant["name"]! as? String
+                self.restaurantLabel.text = restaurant["name"].string!
                 self.spinner.stopAnimating()
                 self.refreshButton.isHidden = false
             })
             search.start()
         }
     }
+
+    //MARK: - MKMapViewDelegate Conformance
+
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer{
-        guard let circle = overlay as? MKCircle else{
-            return MKOverlayRenderer(overlay: overlay)
-        }
-        let renderer = MKCircleRenderer(circle: circle)
-        renderer.fillColor = #colorLiteral(red: 1, green: 0.8323456645, blue: 0.4732058644, alpha: 0.1950009586)
-        renderer.strokeColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
-        renderer.lineWidth = 1
-        return renderer
+        return selectionCircle!.mapView(mapView, rendererFor: overlay)
+    }
+
+    func mapView(_ mapView: MKMapView, annotationView: MKAnnotationView, didChange newState: MKAnnotationViewDragState, fromOldState oldState: MKAnnotationViewDragState) -> Void{
+        selectionCircle?.mapView(mapView, annotationView: annotationView, didChange: newState, fromOldState: oldState)
+    }
+
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView?{
+        return selectionCircle?.mapView(mapView, viewFor: annotation)
+    }
+
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) -> Void{
+        selectionCircle?.mapView(mapView, regionDidChangeAnimated: animated)
+    }
+
+    
+}
+
+extension CLLocationCoordinate2D{
+    static func ==(right: CLLocationCoordinate2D, left: CLLocationCoordinate2D) -> Bool{
+        return right.latitude == left.latitude && right.longitude == left.longitude
     }
 }
